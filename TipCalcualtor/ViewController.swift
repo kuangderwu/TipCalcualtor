889589cbe1d95e1a838412fd62a9845f64552383
Kuang-Der Wu <apple@applede-macbook-air.local>
2018-10-19 09:38:47 +0800

    Initial Commit

---
+++ TipCalcualtor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalcualtor
+//
+//  Created by apple on 2018/10/19.
+//  Copyright © 2018 Kuang-Der Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
